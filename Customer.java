fbcedcb3e039e2b0e37ea538266186d6cb617655
Gift <gift.maputle@ctucareer.co.za>
2023-08-08 13:33:54 +0200

    Add setter method for customer name

--- Customer.java
+++ Customer.java
@@ -8,4 +8,8 @@ public class Customer{
     customerName = name;
     customerAddress = address;
   }
+
+  public void setCustomerName( String customerName ){
+    this.customerName = customerName;
+  }
 }
